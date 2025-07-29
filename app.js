53f45217ed3f204ba99c16f8e3bc08f72b01c82d
jhey2002 <jheimissonsantos100@gmail.com>
2025-07-29 17:10:02 -0300

    enviando arquivos pro servidor remoto

---
+++ app.js
@@ -0,0 +1,16 @@
+function adicionarAmigo()
+{
+
+   let nomes = document.getElementById ('amigo').value;
+   if (nomes == ''){
+    alert ('por favor, digite o nome de uma pessoa')
+}
+   let lista = document.g
+   
+
+   
+}
+
+function sortearAmigo (){
+
+}
