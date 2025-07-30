e63af6245afc62be9606f8a13647215f89add92a
jhey2002 <jheimissonsantos100@gmail.com>
2025-07-30 20:17:52 -0300

    termino do projeto

--- app.js
+++ app.js
@@ -1,16 +1,58 @@
+let grupo = [];
+let random = Math.floor(Math.random() * grupo.length );
 function adicionarAmigo()
 {
 
    let nomes = document.getElementById ('amigo').value;
-   if (nomes == ''){
-    alert ('por favor, digite o nome de uma pessoa')
-}
-   let lista = document.g
+   let listaAmigos = document.getElementById ('listaAmigos');
    
 
-   
-}
+  
+  
+   if (nomes){
+       grupo.push (nomes);
+       const listItem = document.createElement('li');
+       
+       listItem.textContent = nomes; 
+       listaAmigos.appendChild(listItem); 
+        document.getElementById ('amigo').value ='';
+   } else {
+     
+    alert('Por favor, digite um nome válido.');
+   }
 
+   
+   
+   
+
+} 
+   
 function sortearAmigo (){
 
+let resultado = document.getElementById('resultado');
+const listasorteado = document.createElement('li');
+
+embaralha(grupo);
+
+let random = Math.floor(Math.random() * grupo.length );
+let posicao = grupo[random];
+
+if (resultado.textContent === ''){
+    listasorteado.textContent = "0 amigo sorteado é "+ posicao;
+resultado.appendChild(listasorteado);
+listaAmigos.textContent= '';
+}
+
+}
+
+function embaralha(lista) {
+
+    for (let indice = lista.length; indice; indice--) {
+
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+      
+        [lista[indice - 1], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice - 1]];
+    }
 }
